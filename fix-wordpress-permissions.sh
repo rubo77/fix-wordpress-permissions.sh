68e6e1027d4e54312c3e3f762baea375fcf8fbe8
Kyle Skrinak <kyle@skrinakcreative.com>
2014-09-11 19:43:53 -0700

    check args before proceeding

--- fix-wordpress-permissions.sh
+++ fix-wordpress-permissions.sh
@@ -5,21 +5,59 @@
 #
 # Author: Michael Conigliaro <mike [at] conigliaro [dot] org>
 #
-WP_OWNER=www-data # <-- wordpress owner
-WP_GROUP=www-data # <-- wordpress group
-WP_ROOT=$1 # <-- wordpress root directory
-WS_GROUP=www-data # <-- webserver group
+
+WP_OWNER=$1 # <-- wordpress owner
+WP_GROUP=$2 # <-- wordpress group
+WS_GROUP=$2 # <-- webserver group
+WP_ROOT=$3  # <-- wordpress root directory
+
+# Check the arguments before proceeding
+
+# If user, returns number. Not a user, no value
+ISUSER=$(id -u $1 2> /dev/null)
+# Is group in the group file? If so, returns line
+ISGRP=$(egrep -i $2 /etc/group)
+
+if [[ $ISUSER -eq 0 ]]
+  then
+    echo "$1 is not a user"
+    exit 1
+fi
+
+if [[ ${#ISGRP} -eq 0 ]]
+  then
+    echo "$2 is not a group"
+    exit 1
+fi
+
+if [[ ${#3} -eq 0 ]]
+  then
+    echo "No path arguments supplied. Bye."
+    exit 1
+fi
+
+if [[ ! -d $3/wp-admin ]]
+then
+  echo "$3 is not a valid path. Bye."
+  exit 1
+fi
 
 # reset to safe defaults
+echo "Reseting permissions to safe defaults"
+
 find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
 find ${WP_ROOT} -type d -exec chmod 755 {} \;
 find ${WP_ROOT} -type f -exec chmod 644 {} \;
 
 # allow wordpress to manage wp-config.php (but prevent world access)
+echo "Allowing wordpress to manage wp-config.php (but prevent world access)"
+
 chgrp ${WS_GROUP} ${WP_ROOT}/wp-config.php
 chmod 660 ${WP_ROOT}/wp-config.php
 
 # allow wordpress to manage wp-content
+echo "Allowing wordpress to manage wp-content"
+
 find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
 find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
 find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
