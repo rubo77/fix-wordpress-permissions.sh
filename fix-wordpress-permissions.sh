39bac02a1041b221c2de3a8323ae6e651545684b
Ruben Barkow-Kuder <github@r.z11.de>
2020-04-07 01:26:25 +0200

    change order of arguments: path first

--- fix-wordpress-permissions.sh
+++ fix-wordpress-permissions.sh
@@ -3,43 +3,46 @@
 # This script configures WordPress file permissions based on recommendations
 # from http://codex.wordpress.org/Hardening_WordPress#File_permissions
 #
-# Author: Michael Conigliaro <mike [at] conigliaro [dot] org>
-#
+# Authors:
+# Michael Conigliaro <mike [at] conigliaro [dot] org>
+# Kyle Skrinak kyleskrinak
+# 
 
-WP_OWNER=$1 # <-- wordpress owner
-WP_GROUP=$2 # <-- wordpress group
-WS_GROUP=$2 # <-- webserver group
-WP_ROOT=$3  # <-- wordpress root directory
+WP_ROOT=$1  # <-- wordpress root directory
+WP_OWNER=$2 # <-- wordpress owner
+WP_GROUP=$3 # <-- wordpress group
+WWW_GROUP=$3 # <-- webserver group
+
+
+if [[ ${#1} -eq 0 ]]
+then
+  echo "No path arguments supplied. Bye."
+  exit 1
+fi
+
+if [[ ! -d $1/wp-admin ]]
+then
+  echo "$1 is not a valid path. Bye."
+  exit 1
+fi
 
 # Check the arguments before proceeding
 
 # If user, returns number. Not a user, no value
-ISUSER=$(id -u $1 2> /dev/null)
+ISUSER=$(id -u $2 2> /dev/null)
 # Is group in the group file? If so, returns line
-ISGRP=$(egrep -i $2 /etc/group)
+ISGRP=$(egrep -i $3 /etc/group)
 
 if [[ $ISUSER -eq 0 ]]
   then
-    echo "$1 is not a user"
+    echo "$2 is not a user"
     exit 1
 fi
 
 if [[ ${#ISGRP} -eq 0 ]]
   then
-    echo "$2 is not a group"
+    echo "$3 is not a group"
     exit 1
-fi
-
-if [[ ${#3} -eq 0 ]]
-  then
-    echo "No path arguments supplied. Bye."
-    exit 1
-fi
-
-if [[ ! -d $3/wp-admin ]]
-then
-  echo "$3 is not a valid path. Bye."
-  exit 1
 fi
 
 echo "Proceeding with the following assumptions:"
@@ -64,12 +67,12 @@ find ${WP_ROOT} -type f -exec chmod 644 {} \;
 # allow wordpress to manage wp-config.php (but prevent world access)
 echo "Allowing wordpress to manage wp-config.php (but prevent world access)"
 
-chgrp ${WS_GROUP} ${WP_ROOT}/wp-config.php
+chgrp ${WWW_GROUP} ${WP_ROOT}/wp-config.php
 chmod 660 ${WP_ROOT}/wp-config.php
 
 # allow wordpress to manage wp-content
 echo "Allowing wordpress to manage wp-content"
 
-find ${WP_ROOT}/wp-content -exec chgrp ${WS_GROUP} {} \;
+find ${WP_ROOT}/wp-content -exec chgrp ${WWW_GROUP} {} \;
 find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
 find ${WP_ROOT}/wp-content -type f -exec chmod 664 {} \;
