516e932acdfeb94e9ee9760205111708ecd713d3
Ruben Barkow-Kuder <github@r.z11.de>
2020-04-07 03:02:51 +0200

    better comment output when on set -x

--- fix-wordpress-permissions.sh
+++ fix-wordpress-permissions.sh
@@ -101,22 +101,19 @@ if [[ "$NO_CONFIRM" != "1" ]]; then
   echo -e "And we are off!"
 fi
 
-# reset to safe defaults
-echo -e "\n#### Reseting permissions to safe defaults"
-
 set -x
+: ::: Reseting permissions to safe defaults
+
 find ${WP_ROOT} -exec chown ${WP_OWNER}:${WP_GROUP} {} \;
 find ${WP_ROOT} -type d -exec chmod 755 {} \;
 find ${WP_ROOT} -type f -exec chmod 644 {} \;
 
-# allow wordpress to manage wp-config.php (but prevent world access)
-echo -e "\n#### Allowing wordpress to manage wp-config.php (but prevent world access)"
+: ::: Allowing wordpress to manage wp-config.php, but prevent world access
 
 chgrp ${WWW_GROUP} ${WP_ROOT}/wp-config.php
 chmod 660 ${WP_ROOT}/wp-config.php
 
-# allow wordpress to manage wp-content
-echo -e "\n#### Allowing wordpress to manage wp-content"
+: ::: Allowing wordpress to manage wp-content
 
 find ${WP_ROOT}/wp-content -exec chgrp ${WWW_GROUP} {} \;
 find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
